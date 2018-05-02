973f009325c79404986ce2fa55a2d4027c94f102
Ajay Tanwani <ajay.tanwani@berkeley.edu>
2018-05-02 11:29:34 -0700

    repository strucutre

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""
+Setup of Fetch CORE Tools
+"""
+from setuptools import setup
+
+setup(name='core',
+      description='Fetch_Core',
+      author='Ajay Tanwani',
+      author_email='aktanwani@gmail.com',
+      packages=['core'],
+)

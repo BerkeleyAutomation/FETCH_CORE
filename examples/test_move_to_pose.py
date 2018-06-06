More documentation, figured out correct quaternion ordering

examples/test_move_to_pose.py
@@ -11,36 +11,42 @@ import rospy
 DEG_TO_RAD = np.pi / 180
 RAD_TO_DEG = 180 / np.pi
 
+
+def debug_pose_location_rviz():
+    """TODO: figure out locking issues, and what rotations mean"""
+    pose0 = robot.create_grasp_pose(1, 0, 0, 0, intuitive=True)
+    time.sleep(2)
+    pose1 = robot.create_grasp_pose(1, 0, 0, 90*DEG_TO_RAD, intuitive=True)
+    time.sleep(2)
+
+
+def test_motion_planning():
+    """It's helpful if only one pose is created to move to."""
+    pose = robot.create_grasp_pose(0.7, 0, 0.5, 0, intuitive=True)
+    time.sleep(2)
+    robot.move_to_pose(pose, z_offset=0) 
+
+
+def test_inverse_kinematics():
+    """TODO: we haven't tested this"""
+    joints = robot.arm.compute_ik(pose_stamped=ps)
+    if joints:
+        rospy.loginfo('Found IK!\n{}'.format(joints))
+    else:
+        rospy.loginfo('No IK found.')  
+
+
 if __name__ == "__main__":
     rospy.loginfo("Initializing our robot (this may take about 10 seconds) ...")
     robot = Robot_Interface()
     rospy.loginfo("finished Initializing")
     #robot.body_start_pose(start_height=0.15, end_height=0.15)
+    time.sleep(2) # w/out this, we get missing topics when creating poses
 
-    # W/out this, we get missing topics when creating poses
-    time.sleep(2) 
+    # Now test! Comment out as desired.
 
-    # Visualize in rviz to debug positioning and rotation
-    #pose0 = robot.create_grasp_pose(1, 0, 0, 0, intuitive=True)
-    #time.sleep(2)
-    # Ugh, locks??
-    #pose1 = robot.create_grasp_pose(1, 0, 0, 180*DEG_TO_RAD, intuitive=True)
-    #time.sleep(2)
+    debug_pose_location_rviz()
+    #test_motion_planning()
+    #test_inverse_kinematics()
 
-    # The one we actually move to.
-    pose = robot.create_grasp_pose(0.7, 0, 0.5, 0, intuitive=True)
-    time.sleep(2)
-
-    # Check for ability to move to pose via motion planning
-    print("started moving")
-    robot.move_to_pose(pose, z_offset=0) 
-    print("finished moving")
-
-    # Check for reachability via inverse kinematics
-
-    ## joints = robot.arm.compute_ik(pose_stamped=ps)
-    ## if joints:
-    ##     rospy.loginfo('Found IK!\n{}'.format(joints))
-    ## else:
-    ##     rospy.loginfo('No IK found.')
     rospy.spin()

fetch_core/gripper.py
@@ -87,14 +87,14 @@ class Gripper(object):
 
 
     def create_grasp_pose(self, x, y, z, rot, intuitive=False):
-        """Broadcast given pose and return its name
+        """Broadcast given pose and return its name.
 
-        Args: all (x, y, z, rot) are scalars. x, y represent image coordinates,
-        z represents depth, rot represents one single angle (not all Euler
-        angles, not all the quaternion components, etc).
-
-        Unless intuitive=True, then x,y,z,rot are values wrt the odom frame.
-        Keeping this False by default for backwards compatibility.
+        Args: 
+            x,y,z,rot: all are scalars representing the pose, with `rot` as the
+                rotation about the z-axis (in _radians_).
+            intuitive: boolean, should be False except for when we are testing
+                poses, in which case we want to explicitly define a pose w.r.t.
+                the base_link for easier testing.
         """
         self.broadcast_poses([x,y,z], rot, intuitive)
         grasp_name = 'grasp_'+str(self.count)
@@ -115,7 +115,7 @@ class Gripper(object):
             b = tf.transformations.quaternion_from_euler(ai=0.0,aj=0.0,ak=1.57)
             base_rot = tf.transformations.quaternion_multiply(a,b)
             thread.start_new_thread(self.loop_broadcast,(norm_pose,base_rot,rot))
-        rospy.sleep(0.5)
+        rospy.sleep(1.0)
 
 
     def loop_broadcast(self, norm_pose, base_rot, rot_z):
@@ -140,14 +140,14 @@ class Gripper(object):
         """The intuitive way to test out poses.
 
         Specifically, now have position and rot_z be points with respect to the
-        odom frame, so I can directly interpret it rather than assuming the
-        original inputs to creating the poses were camera pixels.
+        base_link frame, so I can directly interpret it.
+
+        grasp_i_k = pose w.r.t base link
+        grasp_k   = pose w.r.t grasp_i_k w/some offset, for better positioning. 
         """
         count = np.copy(self.count)
-        quat0 = (1,0,0,0)
+        quat0 = (0,0,0,1) # Identity rotation, in (x,y,z,w) form, NOT (w,x,y,z)
         quat1 = tf.transformations.quaternion_from_euler(ai=0.0, aj=0.0, ak=rot_z)
-        print(position, rot_z, quat0, quat1, count)
-
         while True:
             self.br.sendTransform(position,
                                   quat0,
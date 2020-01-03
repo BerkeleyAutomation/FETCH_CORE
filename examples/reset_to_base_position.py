b2ce63b096e183399f49ac7fb6037b0618f47f4b
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2020-01-03 11:04:59 -0800

    Quick reset script

--- examples/reset_to_base_position.py
+++ examples/reset_to_base_position.py
@@ -10,11 +10,10 @@ VEL = 0.5
 
 if __name__ == "__main__":
     robot = Robot_Skeleton()
-    
+   
     robot.body_start_pose(start_height=0.20, end_height=0, velocity_factor=VEL)
     rospy.loginfo("Finished moving robot to start pose.")
     # default: robot.body_start_pose()
     robot.head_start_pose(pan=0.0, tilt=45.0*DEG_TO_RAD)
     
-  
     rospy.spin() # yields activity to other threads
